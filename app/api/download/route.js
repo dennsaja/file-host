61d6f13a7d838ad12cb4e82c86bee10afc84d268
Denis <136689391+dennsaja@users.noreply.github.com>
2025-11-28 11:25:46 +0700

    Delete app directory

--- app/api/download/route.js
+++
@@ -1,7 +0,0 @@
-
-export const runtime = 'edge';
-
-export async function GET(req) {
-  const url = new URL(req.url).searchParams.get("url");
-  return Response.redirect(url);
-}
--- app/api/list/route.js
+++
@@ -1,8 +0,0 @@
-
-import { list } from '@vercel/blob';
-export const runtime = 'edge';
-
-export async function GET() {
-  const files = await list();
-  return Response.json(files.blobs);
-}
